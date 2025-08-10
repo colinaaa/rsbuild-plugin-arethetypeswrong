be1c9150b17b684efed303a476e3a4412f9cc075
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-08-10 14:11:44 +0800

    chore: node 18

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
   lib: [
-    { format: "esm", syntax: "es2021", dts: true },
-    { format: "cjs", syntax: "es2021" },
+    { format: "esm", syntax: "es2022", dts: true },
+    { format: "cjs", syntax: "es2022" },
   ],
 });
